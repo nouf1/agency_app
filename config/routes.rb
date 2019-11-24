2554d2eb02ac2cd43937b1d89babb370fff482bf
Moayad Alnuwaysir <moayad.alnu@gmail.com>
2019-11-24 16:20:32 +0300

    Create packages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'packages/index'
+  get 'packages/new'
+  get 'packages/create'
+  get 'packages/show'
+  get 'packages/edit'
+  get 'packages/update'
+  get 'packages/destroy'
   get 'users/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
